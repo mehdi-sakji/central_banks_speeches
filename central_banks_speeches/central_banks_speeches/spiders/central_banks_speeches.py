4d1e264187adf6f9cc3681e3001f7fe3e47aa502
Mehdi Sakji <sakji.mehdi@gmail.com>
2020-08-26 08:28:10 +0000

    Setup date ranges and number of pages.

---
+++ central_banks_speeches/central_banks_speeches/spiders/central_banks_speeches.py
@@ -0,0 +1,13 @@
+import scrapy
+
+class CentralBanksSpeechesScraper(scrapy.Spider):
+    """"""
+
+    name = "central_banks_speeches"
+
+
+    def start_requests(self):
+        """ TODO """
+
+        
+        https://www.bis.org/list/cbspeeches/from_01072019/page_6.htm
